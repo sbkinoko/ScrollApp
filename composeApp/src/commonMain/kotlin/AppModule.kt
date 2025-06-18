bc3cfbb0bbde238bf4c883d4a11b2924f6c1cb99
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-06-18 13:07:34 +0900

    サンプル部分を削除

--- composeApp/src/commonMain/kotlin/AppModule.kt
+++ composeApp/src/commonMain/kotlin/AppModule.kt
@@ -1,23 +1,9 @@
 import org.koin.dsl.module
-import repository.GoodMorningRepository
-import repository.GreetingRepository
-import usecase.GoodMorningUseCase
 import viewmodel.KmpViewModel
 
 val AppModule = module {
-    single<GreetingRepository> {
-        GoodMorningRepository()
-    }
-
-    single<GoodMorningUseCase> {
-        GoodMorningUseCase(
-            repository = get(),
-        )
-    }
 
     single<KmpViewModel> {
-        KmpViewModel(
-            useCase = get(),
-        )
+        KmpViewModel()
     }
 }
