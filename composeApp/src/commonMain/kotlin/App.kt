405bdf703b0051e438b842b9e427a80c0531e81e
sbknk <136720653+sbkinoko@users.noreply.github.com>
2025-06-23 13:04:47 +0900

    入れ子にしてボタンを追加

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,8 +1,12 @@
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,22 +18,52 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 @Preview
 fun App() {
-    LazyColumn(
+    val scrollState = rememberLazyListState()
+
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .frame()
+            .padding(20.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp),
     ) {
-        items(
-            List(50) { it + 1 }
+        LazyColumn(
+            modifier = Modifier.weight(1f)
+                .fillMaxWidth()
+                .frame(),
+            state = scrollState,
         ) {
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .frame(),
-                textAlign = TextAlign.Center,
-                text = "$it",
-                fontSize = 50.sp
-            )
+            items(
+                List(50) { it + 1 }
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .frame(),
+                    textAlign = TextAlign.Center,
+                    text = "$it",
+                    fontSize = 50.sp
+                )
+            }
         }
+
+        Text(
+            text = "↑",
+            modifier = Modifier
+                .fillMaxWidth()
+                .frame()
+                .clickable {},
+            fontSize = 50.sp,
+            textAlign = TextAlign.Center,
+        )
+
+        Text(
+            text = "↓",
+            modifier = Modifier
+                .fillMaxWidth()
+                .frame()
+                .clickable { },
+            fontSize = 50.sp,
+            textAlign = TextAlign.Center,
+        )
     }
 }
